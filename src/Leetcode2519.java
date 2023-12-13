b32a89235096ce0f86b3422676099d3d9d68d513
jerryyummy <461697582@qq.com>
2023-12-13 10:41:53 -0800

    12/13 by yummy

--- src/Leetcode2519.java
+++ src/Leetcode2519.java
@@ -1,2 +1,21 @@
 public class Leetcode2519 {
+    public int kBigIndices(int[] nums, int k){
+        int n = nums.length;
+        FenwickTree pre = new FenwickTree(n);
+        FenwickTree suff = new FenwickTree(n);
+        int[][] count = new int[n+1][2];
+        for (int i = 0; i < n; i++) {
+            count[i][0] = pre.totalSum(nums[i]-1);
+            pre.update(nums[i],1);
+            count[n-i-1][1] = suff.totalSum(nums[n-i-1]-1);
+            suff.update(nums[n-i-1],1);
+        }
+
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            if (count[i][0]>=k && count[i][1]>=k) res++;
+        }
+
+        return res;
+    }
 }
--- src/Leetcode84.java
+++ src/Leetcode84.java
@@ -1,2 +1,21 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Leetcode84 {
+    public int largestRectangleArea(int[] heights){
+        int[] tmp = new int[heights.length + 2];
+        System.arraycopy(heights, 0, tmp, 1, heights.length);
+        Deque<Integer> stack = new ArrayDeque<>();
+        int res = 0;
+        for (int i = 0; i < tmp.length; i++) {
+
+            while (!stack.isEmpty() && tmp[stack.peek()]>tmp[i]){//保持递增
+                int high = tmp[stack.pop()];
+                res = Math.max((i-stack.peek()-1)*high,res);
+            }
+            stack.push(i);
+
+        }
+        return res;
+    }
 }
