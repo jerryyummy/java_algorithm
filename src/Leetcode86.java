47c45c73a8c1100cef9503fe5cf6df234fcf8b4e
jerryyummy <461697582@qq.com>
2023-12-23 17:57:16 -0800

    12/22 by yummy

---
+++ src/Leetcode86.java
@@ -0,0 +1,31 @@
+public class Leetcode86 {
+    public ListNode partition(ListNode head, int x) {
+        ListNode before_head = new ListNode(0);
+        ListNode before = before_head;
+        ListNode after_head = new ListNode(0);
+        ListNode after = after_head;
+
+        while (head != null) {
+
+            // If the original list node is lesser than the given x,
+            // assign it to the before list.
+            if (head.val < x) {
+                before.next = head;
+                before = before.next;
+            } else {
+                // If the original list node is greater or equal to the given x,
+                // assign it to the after list.
+                after.next = head;
+                after = after.next;
+            }
+
+            // move ahead in the original list
+            head = head.next;
+        }
+
+        after.next = null;
+        before.next = after_head.next;
+
+        return before_head.next;
+    }
+}
