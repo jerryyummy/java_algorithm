5e63cfb0edb726e24b401a70468f3aec036f0db3
jerryyummy <461697582@qq.com>
2023-12-28 00:02:19 -0800

    12/26 by yummy

---
+++ src/Leetcode2402.java
@@ -0,0 +1,46 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Leetcode2402 {
+    public int mostBooked(int n, int[][] meetings) {
+        int[] meetingCount = new int[n];
+        Queue<long[]> usedRooms = new PriorityQueue<long[]>((a, b) -> a[0] != b[0] ? Long.compare(a[0], b[0]) : Long.compare(a[1], b[1]));
+        Queue<Integer> unusedRooms = new PriorityQueue<Integer>();
+
+        for (int i = 0; i < n; i++) {
+            unusedRooms.offer(i);
+        }
+
+        Arrays.sort(meetings, (a, b) -> a[0] != b[0] ? Integer.compare(a[0], b[0]) : Integer.compare(a[1], b[1]));
+
+        for (int[] meeting : meetings) {
+            int start = meeting[0], end = meeting[1];
+
+            while (!usedRooms.isEmpty() && usedRooms.peek()[0] <= start) {
+                int room = (int) usedRooms.poll()[1];
+                unusedRooms.offer(room);
+            }
+
+            if (!unusedRooms.isEmpty()) {
+                int room = unusedRooms.poll();
+                usedRooms.offer(new long[]{end, room});
+                meetingCount[room]++;
+            } else {
+                long roomAvailabilityTime = usedRooms.peek()[0];
+                int room = (int) usedRooms.poll()[1];
+                usedRooms.offer(new long[]{roomAvailabilityTime + end - start, room});
+                meetingCount[room]++;
+            }
+        }
+
+        int maxMeetingCount = 0, maxMeetingCountRoom = 0;
+        for (int i = 0; i < n; i++) {
+            if (meetingCount[i] > maxMeetingCount) {
+                maxMeetingCount = meetingCount[i];
+                maxMeetingCountRoom = i;
+            }
+        }
+
+        return maxMeetingCountRoom;
+    }
+}
