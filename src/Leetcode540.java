5c67ccab37b9dcf9635478b6dd08c32e1c40f247
jerryyummy <461697582@qq.com>
2024-01-10 16:28:40 -0800

    1/9/2024 by yummy

---
+++ src/Leetcode540.java
@@ -0,0 +1,26 @@
+public class Leetcode540 {
+    public int singleNonDuplicate(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            boolean halvesAreEven = (right - mid) % 2 == 0;
+            if (nums[mid + 1] == nums[mid]) {
+                if (halvesAreEven) {
+                    left = mid + 2;
+                } else {
+                    right = mid - 1;
+                }
+            } else if (nums[mid - 1] == nums[mid]) {
+                if (halvesAreEven) {
+                    right = mid - 2;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                return nums[mid];
+            }
+        }
+        return nums[left];
+    }
+}
