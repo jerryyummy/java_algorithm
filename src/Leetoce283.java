3652e884570476f94c588b661f9e8416540867d9
jerryyummy <461697582@qq.com>
2024-01-15 17:49:33 -0800

    1/15/2024 by yummy

---
+++ src/Leetoce283.java
@@ -0,0 +1,20 @@
+public class Leetoce283 {
+    public void moveZeroes(int[] nums) {
+        int i = 0, j = 0;
+        while (j<nums.length){
+            while (j<nums.length && nums[j]==0){
+                j++;
+            }
+            while (i<j && nums[i]!=0){
+                i++;
+            }
+            if(j<nums.length && i<j) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+            }
+            j++;
+            i++;
+        }
+    }
+}
