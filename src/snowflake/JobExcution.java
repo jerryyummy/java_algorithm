a1f14006274e6433a6b8af797b65b1d149f8b119
jerryyummy <461697582@qq.com>
2024-01-24 18:03:32 -0800

    1/24/2024 by yummy

--- src/snowflake/JobExcution.java
+++ src/snowflake/JobExcution.java
@@ -2,11 +2,40 @@ package snowflake;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
+import java.util.Random;
 
 public class JobExcution {
+  public int getMinimumOperations(int[] executionTime, int x, int y){
+    int n = executionTime.length;
+    Arrays.sort(executionTime);
+    int i = 0;
+    while (executionTime[n-1]>0){
+      i++;
+      executionTime[n-1]-=x;
+      int left = 0, right = n-2;
+      while (left<right){
+        int mid = (left+right)/2;
+        if(executionTime[mid]<0) left = mid+1;
+        else right = mid;
+      }
+      for (int j = left; j < n-1; j++) {
+        executionTime[j]-=y;
+      }
+      Arrays.sort(executionTime);
+    }
+    return i;
+  }
+
+  public static void main(String[] args) {
+    JobExcution solution = new JobExcution();
+    int[] temp = new int[100000];
+    Random random = new Random();
+    for (int i = 0; i < 100000; i++) {
+      temp[i] = random.nextInt((int) 1e9);
+    }
+    int x = random.nextInt((int) 1e9);
+    int y = x-2>0? x-2:1;
+    System.out.println(solution.getMinimumOperations(temp, x, y));
+  }
 
 }
