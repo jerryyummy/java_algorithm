1396cc080c79d695620544feb19200a150560424
jerryyummy <461697582@qq.com>
2024-03-30 23:07:13 -0700

    3/29/2024 by yummy

--- src/Leetcode1049.java
+++ src/Leetcode1049.java
@@ -1,3 +1,23 @@
 public class Leetcode1049 {
+  public int lastStoneWeightII(int[] stones) {
+    int totalSum = 0;
+    for (int stone : stones) {
+      totalSum += stone;
+    }
 
+    boolean[] dp = new boolean[totalSum / 2 + 1];
+    dp[0] = true;
+
+    for (int stone : stones) {
+      for (int j = totalSum / 2; j >= stone; j--) {
+        dp[j] = dp[j] || dp[j - stone];
+      }
+    }
+
+    for (int j = totalSum / 2; ; j--) {
+      if (dp[j]) {
+        return totalSum - 2 * j;
+      }
+    }
+  }
 }
