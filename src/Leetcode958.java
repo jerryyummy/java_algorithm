92d141e8cd3cc91a3af44a99989b83171747ff44
jerryyummy <461697582@qq.com>
2024-05-17 15:16:32 +0800

    5/16/2024 by yummy

--- src/Leetcode958.java
+++ src/Leetcode958.java
@@ -25,15 +25,16 @@ public class Leetcode958 {
    */
 //  int n = 0, p = 0;
 //  public boolean isCompleteTree(TreeNode root) {
-//    if(!dfs(root,1)) return false;
+//    dfs(root,1);
 //    return n == p;
 //  }
-//  public boolean dfs(TreeNode root , int k) //k是当前节点编号
+//  public void dfs(TreeNode root , int k) //k是当前节点编号
 //  {
-//    if(root == null) return true;  //递归到了叶子节点
-//    if(k > 100) return false;
-//    n++;  p = Math.max(p,k); //记录节点数和最大节点编号值
-//    return dfs(root.left,2*k)&&dfs(root.right,2*k + 1); //递归左右子树
+//    if(root == null) return;  //递归到了叶子节点
+//    n++;
+//    p = Math.max(p,k); //记录节点数和最大节点编号值
+//    dfs(root.left,2*k);
+//    dfs(root.right,2*k + 1); //递归左右子树
 //  }
 
 }
