f61e978783a30ecd4784658b79c9cc033736f40d
jerryyummy <461697582@qq.com>
2024-06-01 22:18:32 +0800

    6/1/2024 by yummy for snowflake

--- src/snowflake/NumWays.java
+++ src/snowflake/NumWays.java
@@ -1,10 +1,41 @@
 package snowflake;
 
-import java.util.List;
+import java.util.*;
 
 //Leetcode 1639
 public class NumWays {
-  public int numWays(List<String> words, String target) {
-    return 0;
+  static final int mod = 1_000_000_007;
+
+  static int numWays(List<String> words, String target) {
+    int m = words.size(), n = words.get(0).length(), k = target.length();
+
+    //预处理 在第i个位置 有多少个'a'+j字符
+    int[][] cnts = new int[n][26];
+    for (String s : words) {
+      for (int i = 0; i < n; i++) {
+        cnts[i][s.charAt(i) - 'a']++;
+      }
+    }
+    //状态转移 dp[i][j] = dp[i][j-1] + dp[i-1][j-1] * cnts[j][ts[i]-'a']
+    int[][] dp = new int[k + 1][n + 1];
+    Arrays.fill(dp[0], 1);
+
+    for (int i = 0; i < k; i++) {
+      int idx = target.charAt(i) - 'a';
+      int t = n - (k - 1 - i);
+      for (int j = i; j < t; j++) {
+        long dpij = dp[i + 1][j] + (long) dp[i][j] * cnts[j][idx];
+        dp[i + 1][j + 1] = (int) (dpij % mod);
+      }
+    }
+    return dp[k][n];
+  }
+
+  public static void main(String[] args) {
+    List<String> words = new ArrayList<>();
+    words.add("ace");
+    words.add("dgf");
+    words.add("ade");
+    System.out.println(numWays(words,"ae"));
   }
 }
--- src/snowflake/RadioWaves.java
+++ src/snowflake/RadioWaves.java
@@ -5,62 +5,47 @@ import java.util.*;
 //经典的树上俩点最长距离的问题，就是多加了一个constraint，
 // 一遍dfs即可把通过任意节点的最长路径全算出来，其中的最大值就是结果了。线性时间复杂度
 public class RadioWaves {
-  private HashMap<Integer, List<Integer>> buildGraph(int network_nodes, int[] network_from, int[] network_to) {
-    HashMap<Integer, List<Integer>> graph = new HashMap<>();
-    for (int i = 0; i < network_from.length; i++) {
+  HashMap<Integer, List<Integer>> graph = new HashMap<>();
+  int res = 0;
+
+  private void buildGraph(int network_nodes, int[] network_from, int[] network_to) {
+    for (int i = 0; i < network_nodes-1; i++) {
       graph.computeIfAbsent(network_from[i], k -> new ArrayList<>()).add(network_to[i]);
       graph.computeIfAbsent(network_to[i], k -> new ArrayList<>()).add(network_from[i]);
     }
-    return graph;
-  }
-
-  // Pair class to keep track of node and its distance
-  private static class Pair {
-    int node;
-    int dist;
-    Pair(int node, int dist) {
-      this.node = node;
-      this.dist = dist;
-    }
-  }
-
-  private Pair bfs(int startNode, HashMap<Integer, List<Integer>> graph, int[] frequency) {
-    Queue<Integer> queue = new LinkedList<>();
-    boolean[] visited = new boolean[frequency.length + 1];
-    queue.offer(startNode);
-    visited[startNode] = true;
-
-    Pair farthest = new Pair(startNode, 0);
-
-    while (!queue.isEmpty()) {
-      int current = queue.poll();
-      for (int neighbor : graph.getOrDefault(current, Collections.emptyList())) {
-        if (!visited[neighbor] && Math.abs(frequency[current - 1] - frequency[neighbor - 1]) <= 1) {
-          visited[neighbor] = true;
-          queue.offer(neighbor);
-          farthest = new Pair(neighbor, farthest.dist + 1);
-        }
-      }
-    }
-    return farthest; // Return both node and its distance
   }
 
   public int calculateMax(int network_nodes, int[] network_from, int[] network_to, int[] frequency) {
-    HashMap<Integer, List<Integer>> graph = buildGraph(network_nodes, network_from, network_to);
+     buildGraph(network_nodes, network_from, network_to);
+     dfs(1, frequency, 0);
+     return res;
+  }
 
-    // Perform the first BFS from any node, just pick 1 for simplicity
-    Pair firstBfsResult = bfs(1, graph, frequency);
-
-    // Perform the second BFS from the farthest node found from the first BFS
-    Pair secondBfsResult = bfs(firstBfsResult.node, graph, frequency);
-
-    // The distance found in the second BFS is the longest path with frequency constraints
-    return secondBfsResult.dist;
+  private int dfs(int node, int[] frequency, int parent) {
+    int first = 0;
+    int second = 0;
+    for (int next:graph.get(node)){
+      if (next != parent){
+        if (Math.abs(frequency[next-1] - frequency[node-1]) > 1){
+          dfs(next, frequency, node);
+        }else{
+          int temp = dfs(next, frequency, node);
+          if(temp>first){
+            second = first;
+            first = temp;
+          }else if(temp > second){
+            second = temp;
+          }
+        }
+      }
+    }
+    res = Math.max(res, first+second);
+    return first+1;
   }
 
   public static void main(String[] args) {
     RadioWaves solution = new RadioWaves();
-    int longestDistance = solution.calculateMax(5, new int[]{1, 2, 3, 1}, new int[]{2, 3, 4, 5}, new int[]{3, 2, 3, 2, 2});
+    int longestDistance = solution.calculateMax(10, new int[]{1, 1, 1, 2, 3, 5, 5, 7, 8}, new int[]{6, 2, 3, 5, 4, 7, 8, 9, 10}, new int[]{1, 3, 2, 3, 2, 2, 1, 1, 2, 2});
     System.out.println("The longest distance is: " + longestDistance);
   }
 }
