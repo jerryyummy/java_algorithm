77db7dddc7d1b84dce97e29dfc767bc65fa430eb
jerryyummy <461697582@qq.com>
2024-06-08 23:55:07 +0800

    6/8/2024 by yummy for snowflake

--- src/huawei/OpeningUpWaterways.java
+++ src/huawei/OpeningUpWaterways.java
@@ -1,5 +1,9 @@
 package huawei;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class OpeningUpWaterways {
@@ -27,7 +31,38 @@ public class OpeningUpWaterways {
 
   private static int calculateMaxNonIntersectingPaths(int[] southCities, int[] northCities) {
     int count = 0;
+    List<Line> lines = new ArrayList<>();
+    for (int i = 0; i < southCities.length; i++) {
+      lines.add(new Line(southCities[i],northCities[i]));
+    }
+
+    Collections.sort(lines, new Comparator<Line>() {
+      @Override
+      public int compare(Line o1, Line o2) {
+        return o1.up-o2.up;
+      }
+    });
+    int[] dp = new int[lines.size()+1];
+    for (int i = 1; i <= northCities.length; i++) {
+      dp[i] = 1;
+      for (int j = 1; j < i; j++) {
+        if (lines.get(i-1).down > lines.get(j-1).down) {
+          dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
+      }
+      count = Math.max(count, dp[i]);
+    }
 
     return count;
+  }
+
+  static class Line{
+    int up;
+    int down;
+
+    public Line(int up, int down) {
+      this.up = up;
+      this.down = down;
+    }
   }
 }
