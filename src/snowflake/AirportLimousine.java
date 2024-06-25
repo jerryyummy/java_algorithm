86f69fe65e96329c72721b0cd03febef392f90cc
jerryyummy <461697582@qq.com>
2024-06-25 23:27:32 +0800

    6/23/2024 by yummy for snowflake

--- src/snowflake/AirportLimousine.java
+++ src/snowflake/AirportLimousine.java
@@ -1,92 +1,18 @@
 package snowflake;
 
-/*
-public class AirportLimousine {
-  public static void main(String[] args) {
-    int[][] mat = {
-        {0, 1, -1},
-        {1, 0, 1},
-        {1, 1, 1}
-    };
-    System.out.println(collectMax(mat)); // 输出示例
-  }
-
-  public static int collectMax(int[][] mat) {
-    int n = mat.length;
-    int m = mat[0].length;
-    int passengers = 0;
-
-    // 计算总共的乘客数量
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        if (mat[i][j] == 1) {
-          passengers++;
-        }
-      }
-    }
-
-    int finished = 0;
-    while (finished < passengers) {
-      boolean[][] visited = new boolean[n][m];
-      int temp1 = dfs(mat, new int[]{0, 0}, new int[]{n - 1, m - 1}, visited, new int[][]{{0, 1}, {1, 0}});
-      if (temp1 == 0) break;
-      else {
-        finished += temp1;
-        if (finished == passengers) break;
-      }
-      visited = new boolean[n][m]; // 重置访问矩阵
-      int temp2 = dfs(mat, new int[]{n - 1, m - 1}, new int[]{0, 0}, visited, new int[][]{{0, -1}, {-1, 0}});
-      finished += temp2;
-      if (finished == passengers) break;
-    }
-    return finished;
-  }
-
-  public static int dfs(int[][] mat, int[] start, int[] end, boolean[][] visited, int[][] directions) {
-    // 将起始位置标记为已访问
-    visited[start[0]][start[1]] = true;
-    int res = mat[start[0]][start[1]] == 1 ? 1 : 0;
-    mat[start[0]][start[1]] = 0;
-    // 如果当前位置就是终点，返回找到的乘客数量
-    if (start[0] == end[0] && start[1] == end[1]) {
-      return res;
-    }
-
-    // 遍历所有可能的下一步位置
-    for (int[] direction : directions) {
-      int nextX = start[0] + direction[0];
-      int nextY = start[1] + direction[1];
-
-      // 检查新位置是否在矩阵范围内且未被访问过
-      if (nextX >= 0 && nextX < mat.length && nextY >= 0 && nextY < mat[0].length && !visited[nextX][nextY] && mat[nextX][nextY] >= 0) {
-        int temp = dfs(mat, new int[]{nextX, nextY}, end, visited, directions);
-        if (temp >= 1) {
-          return res + temp;
-        }
-      }
-    }
-
-    // 回溯，将状态重置
-    visited[start[0]][start[1]] = false;
-    mat[start[0]][start[1]] = res == 1 ? 1 : 0; // 恢复原状态
-    return 0;
-  }
-}
- */
-
 import java.util.Arrays;
 
 public class AirportLimousine {
 
   public static int collectMax(int[][] grid) {
     int N = grid.length;
-    int[][] dp = new int[N][N];
+    int[][] dp = new int[N][N];//存储到达每个点时的最大价值
     for (int[] row: dp) {
       Arrays.fill(row, Integer.MIN_VALUE);
     }
     dp[0][0] = grid[0][0];
 
-    for (int t = 1; t <= 2*N - 2; ++t) {
+    for (int t = 1; t <= 2*N - 2; ++t) {//这是因为从左上角到右下角需要走N-1步向右和N-1步向下，共2N-2步
       int[][] dp2 = new int[N][N];
       for (int[] row: dp2) {
         Arrays.fill(row, Integer.MIN_VALUE);
@@ -101,7 +27,7 @@ public class AirportLimousine {
           if (i != j) {
             val += grid[j][t - j];
           }
-          for (int pi = i - 1; pi <= i; ++pi) {
+          for (int pi = i - 1; pi <= i; ++pi) {//这是(i, j)在上一步(t-1)可能的来源位置
             for (int pj = j - 1; pj <= j; ++pj) {
               if (pi >= 0 && pj >= 0) {
                 dp2[i][j] = Math.max(dp2[i][j], dp[pi][pj] + val);
@@ -113,15 +39,15 @@ public class AirportLimousine {
       dp = dp2;
     }
     return Math.max(0, dp[N - 1][N - 1]);
-    // write your code here
   }
 
   public static void main(String[] args) {
     int[][] mat = {
-        {0, 1},
-        {-1,0},
+        {0, 1,1},
+        {1,0,1},
+        {1,1,1}
     };
-    System.out.println(collectMax(mat)); // 输出应该为 1
+    System.out.println(collectMax(mat));
   }
 }
 
--- src/snowflake/UnequalElements.java
+++ src/snowflake/UnequalElements.java
@@ -1,20 +1,71 @@
 package snowflake;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class UnequalElements {
 
   // Method to find the maximum length of subsequence with unequal adjacent elements
-  public static int findMaxLength(int[] skills, int k) {
-    return 0;
+  public static int findMaxLength(List<Integer> skills, int k) {
+    List<List<Integer>> merged = mergeConsecutiveSubsequences(skills);
+    int[][] dp = new int[merged.size()][k+1];
+    for (int i = 0; i < merged.size(); i++) {
+      dp[i][0] = merged.get(i).get(1);
+    }
+    int res = 0;
+    for (int i = 1; i < merged.size(); i++) {
+      int[][] temp = dp.clone();
+      for (int j = 0; j <= k; j++) {
+        for (int l = 0; l < i; l++) {
+          if (Objects.equals(merged.get(l).get(0), merged.get(i).get(0))){
+            temp[i][j] = Math.max(temp[i][j], dp[l][j]+merged.get(i).get(1));
+          }else{
+            if (j>0){
+              temp[i][j] = Math.max(temp[i][j], dp[l][j-1]+merged.get(i).get(1));
+            }
+          }
+        }
+      }
+      dp = temp;
+    }
+
+    for (int i = 0; i < merged.size(); i++) {
+      for (int j = 0; j <= k; j++) {
+        res = Math.max(res, dp[i][j]);
+      }
+    }
+    return res;
+  }
+
+  public static List<List<Integer>> mergeConsecutiveSubsequences(List<Integer> skills) {
+    if (skills.isEmpty()) {
+      return new ArrayList<>();
+    }
+    List<List<Integer>> res = new ArrayList<>();
+    int currentNum = skills.get(0);
+    int count = 1;
+    for (int i = 1; i < skills.size(); i++) {
+      if (skills.get(i) == currentNum) {
+        count++;
+      }else{
+        res.add(Arrays.asList(currentNum, count));
+        currentNum = skills.get(i);
+        count = 1;
+      }
+    }
+    res.add(Arrays.asList(currentNum, count));
+    return res;
   }
 
   // Main method for testing the algorithm
   public static void main(String[] args) {
-    int[] skills = {1, 2, 3, 2, 1};
+    int[] skills = {1, 1, 2, 3, 2, 1};
     int k = 2;
-    System.out.println("Maximum length of subsequence: " + findMaxLength(skills, k));
+    System.out.println("Maximum length of subsequence: " + findMaxLength(Arrays.stream(skills).boxed().collect(
+        Collectors.toList()), k));
   }
 }
 
