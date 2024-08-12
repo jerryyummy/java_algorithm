23e61e7b265408caa6a29bd69a8132105b92233c
jerryyummy <461697582@qq.com>
2024-08-13 00:38:11 +0800

    8/11/2024 by yummy

--- src/tt/CardPacket.java
+++ src/tt/CardPacket.java
@@ -1,5 +1,30 @@
 package tt;
 
-public class CardPacket {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+public class CardPacket {
+  public int cardPackets(List<Integer> cardTypes){
+    int n = cardTypes.size();
+    int maxCard = Collections.max(cardTypes);
+    int addtional = Integer.MAX_VALUE;
+    for (int target = 2; target<= maxCard; target++) {
+      int res = 0;
+      for (int i = 0; i < n; i++) {
+        if(cardTypes.get(i) % target != 0) {
+          res += target - (cardTypes.get(i) % target);
+        }
+      }
+      addtional = Math.min(addtional, res);
+    }
+    return addtional;
+  }
+
+  public static void main(String[] args) {
+    CardPacket packet = new CardPacket();
+    List<Integer> cardTypes = new ArrayList<>(Arrays.asList(4,7,5,11,15));
+    System.out.println(packet.cardPackets(cardTypes));
+  }
 }
--- src/tt/MaxGoodSubArrayLength.java
+++ src/tt/MaxGoodSubArrayLength.java
@@ -1,5 +1,49 @@
 package tt;
 
-public class MaxGoodSubArrayLength {
+import java.util.*;
 
+public class MaxGoodSubArrayLength {
+  public static int getMaxSubArrayLength(int limit, List<Integer> financialMetrics) {
+    int n = financialMetrics.size();
+    int[] left = new int[n];
+    int[] right = new int[n];
+    Stack<Integer> s = new Stack<>();
+
+    for (int i = 0; i < n; i++) {
+      while (!s.isEmpty() && financialMetrics.get(s.peek()) >= financialMetrics.get(i)) {
+        s.pop();
+      }
+      left[i] = s.isEmpty() ? 0 : s.peek() + 1;
+      s.push(i);
+    }
+
+    s.clear();
+
+    for (int i = n - 1; i >= 0; i--) {
+      while (!s.isEmpty() && financialMetrics.get(s.peek()) >= financialMetrics.get(i)) {
+        s.pop();
+      }
+      right[i] = s.isEmpty() ? n - 1 : s.peek() - 1;
+      s.push(i);
+    }
+
+    int maxLength = -1;
+
+    for (int i = 0; i < n; i++) {
+      int len = right[i] - left[i] + 1;
+      long te = (long) financialMetrics.get(i) * (long) len;
+      if (te > (long) limit) {
+        maxLength = Math.max(maxLength, len);
+      }
+    }
+
+    return maxLength;
+  }
+
+  public static void main(String[] args) {
+    ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1, 3, 4, 3, 1));
+    int limit = 6;
+    int result = getMaxSubArrayLength(limit, arr);
+    System.out.println(result);
+  }
 }
