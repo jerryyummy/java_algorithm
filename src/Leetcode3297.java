fb84814f7a338b02ac1c2055507dc282d77fe1c0
jerryyummy <461697582@qq.com>
2024-09-22 12:24:01 +0800

    9/22/2024 by yummy

---
+++ src/Leetcode3297.java
@@ -0,0 +1,36 @@
+public class Leetcode3297 {
+  public static long validSubstringCount(String word1, String word2) {
+    int[] v = new int[26];
+    for (char c : word2.toCharArray()) {
+      v[c - 'a']++;
+    }
+
+    int[] cnt = new int[26];
+    int start = 0;
+    int k = word2.length();
+    long count = 0;
+
+    for (int i = 0; i < word1.length(); i++) {
+      char curr = word1.charAt(i);
+      if (v[curr - 'a'] > 0) {
+        if (cnt[curr - 'a'] < v[curr - 'a']) {
+          k--;
+        }
+      }
+
+      cnt[curr - 'a']++;
+
+      while (k == 0) {
+        count += word1.length() - i;
+        char pre = word1.charAt(start);
+        cnt[pre - 'a']--;
+        if (v[pre - 'a'] > 0 && cnt[pre - 'a'] < v[pre - 'a']) {
+          k++;
+        }
+        start++;
+      }
+    }
+
+    return count;
+  }
+}
