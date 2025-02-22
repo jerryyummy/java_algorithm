bc0cce8c572b606d57e9d2add2251ec87ca399a1
jerryyummy <461697582@qq.com>
2025-02-22 10:12:22 -0800

    2/21/2025 by yummy

--- src/amazon/MaxRewardPoints.java
+++ src/amazon/MaxRewardPoints.java
@@ -1,39 +1,25 @@
 package amazon;
 
-import java.util.PriorityQueue;
+import java.util.Collections;
+import java.util.List;
 
 public class MaxRewardPoints {
-    public long getMaxRewardPoints(int[] reward) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+    public static long findMaxPoints(List<Integer> points) {
+        // Write your code here
+        Collections.sort(points);
+        int n = points.size();
+        long ans = 0;
 
-        for (int r : reward) {
-            if (r > 0) {
-                maxHeap.add(r);
+        // Traverse array from largest to smallest
+        for (int i = 0; i < n; i++) {
+            int currentValue = points.get(n - 1 - i); // Access from end since Java Arrays.sort is ascending
+            if (currentValue - i > 0) {
+                ans += currentValue - i;
+            } else {
+                break;
             }
         }
-
-        long totalPoints = 0;
-
-        while (!maxHeap.isEmpty()) {
-            int maxReward = maxHeap.poll();
-            totalPoints += maxReward;
-            PriorityQueue<Integer> tempHeap = new PriorityQueue<>((a, b) -> b - a);
-            while (!maxHeap.isEmpty()) {
-                int reducedReward = maxHeap.poll() - 1;
-                if (reducedReward > 0) {
-                    tempHeap.add(reducedReward);
-                }
-            }
-
-            maxHeap = tempHeap;
-        }
-
-        return totalPoints;
+        return ans;
     }
 
-    public static void main(String[] args) {
-        MaxRewardPoints solution = new MaxRewardPoints();
-        int[] reward = {5, 5,5};
-        System.out.println(solution.getMaxRewardPoints(reward)); // 输出 7
-    }
 }
